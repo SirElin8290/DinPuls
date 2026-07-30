7dacc8de125acef187094889ae1f578c2f51e7c3
SirElin8290 <soren.johansson.7@gmail.com>
2026-07-30 09:06:25 +0200

    Sport 0.20.8: förenkla script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* =========================================================
-   DINPULS.SE v0.20.7
+   DINPULS.SE v0.20.8
    Central kommunmotor, komponenter och datamoduler
 ========================================================= */
 
-const DINPULS_VERSION = "0.20.7";
+const DINPULS_VERSION = "0.20.8";
 const DEFAULT_MUNICIPALITY = "Åmål";
 
 const componentNames = [
