45ba66bc93cc2f3625ab477aaaa5b6644b66a7a6
SirElin8290 <soren.johansson.7@gmail.com>
2026-08-09 13:24:41 +0200

    Förbättra sidfoten på mobil

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* =========================================================
-   DINPULS.SE v0.21.13
+   DINPULS.SE v0.21.14
    Central kommunmotor, komponenter och datamoduler
 ========================================================= */
 
-const DINPULS_VERSION = "0.21.13";
+const DINPULS_VERSION = "0.21.14";
 const DEFAULT_MUNICIPALITY = window.DinPulsMunicipalityState?.DEFAULT_NAME || "Åmål";
 const STOCKHOLM_TIME_ZONE = "Europe/Stockholm";
 
