a5b3116f0c9bb914c04bcfaac938e546d8eabf09
SirElin8290 <soren.johansson.7@gmail.com>
2026-08-09 13:41:54 +0200

    Flytta startsideguiden till sidhuvudet

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* =========================================================
-   DINPULS.SE v0.21.15
+   DINPULS.SE v0.21.16
    Central kommunmotor, komponenter och datamoduler
 ========================================================= */
 
-const DINPULS_VERSION = "0.21.15";
+const DINPULS_VERSION = "0.21.16";
 const DEFAULT_MUNICIPALITY = window.DinPulsMunicipalityState?.DEFAULT_NAME || "Åmål";
 const STOCKHOLM_TIME_ZONE = "Europe/Stockholm";
 
